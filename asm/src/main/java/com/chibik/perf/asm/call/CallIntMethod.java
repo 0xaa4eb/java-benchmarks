ac4fd935b2a9613ac06d9462db1071c4a1ef0494
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-08-10 19:55:08 +0300

    ASM for `this` address passing

--- asm/src/main/java/com/chibik/perf/asm/call/CallIntMethod.java
+++ asm/src/main/java/com/chibik/perf/asm/call/CallIntMethod.java
@@ -8,9 +8,24 @@ import org.openjdk.jmh.annotations.*;
 @PrintAssembly
 public class CallIntMethod {
 
+    private static final Callable INSTANCE = new Callable();
+
     @Benchmark
-    public void callIntIntIntConsumer() {
+    public void callThisIntIntIntConsumer() {
         intIntConsumer(0xdead, 0xbeef, 0xf00d);
+    }
+
+    @Benchmark
+    public void callOtherInstanceIntIntIntConsumer() {
+        INSTANCE.intIntConsumer(0xdead, 0xbeef, 0xf00d);
+    }
+
+    public static class Callable {
+
+        @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+        public void intIntConsumer(int v1, int v2, int v3) {
+
+        }
     }
 
     @CompilerControl(CompilerControl.Mode.DONT_INLINE)
